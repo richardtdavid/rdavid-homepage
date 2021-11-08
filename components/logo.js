2795eb920d812a95e56c080e3407fa62b581ea42
richarddavid <driichard@gmail.com>
2021-11-07 20:32:50 -0600

    addbody section

--- components/logo.js
+++ components/logo.js
@@ -4,14 +4,16 @@ import { Text, useColorModeValue } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
 const LogoBox = styled.span`
-  font-weight: bold
+  font-weight: bold;
   font-size: 18px;
   display: inline-flex;
   align-items: center;
   height: 30px;
   line-height: 20px;
   padding: 10px;
-
+  img {
+    transition: 200ms ease;
+  }
   &:hover img {
     transform: rotate(20deg);
   }
@@ -23,10 +25,10 @@ const Logo = () => {
     <Link href="/">
       <a>
         <LogoBox>
-          <Image src={mountainImg} width={40} height={40} alt="logo" />
+          <Image src={mountainImg} width={20} height={20} alt="logo" />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-            fontFamily="M PLUS Rounded 1c"
+            fontFamily="M PLUS Rounded 1c, sans-serif"
             fontWeight="bold"
             ml={3}
           >
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
-import { Container, Box, Heading, Image } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import {
+  Link,
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+  Button
+} from '@chakra-ui/react';
+import Section from '../components/section';
+import Paragraph from '../components/paragraph';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { BioInfo, BioSection } from '../components/bio';
 
 const page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" mb={6} p={3} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        mb={6}
+        p={3}
+        align="center"
+      >
         Hello, I&apos;m a full-stack developer based in minneapolis!
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -26,6 +45,54 @@ const page = () => {
           alt="profile-image"
         />
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          {' '}
+          Work{' '}
+        </Heading>
+        <Paragraph>
+          Richard is a full stack developer based out of Minneapolis, Minnesota
+          with a passion fro building services that improve quality of life. He
+          has experience in planning, designing, launching and solving real-life
+          problems with code. When not infront of a computer, he can be found
+          taking{' '}
+          <NextLink href="https://500px.com/p/riichard?view=photos">
+            <Link>photos</Link>
+          </NextLink>
+          .
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioInfo>2004</BioInfo>
+          Moved to USA
+        </BioSection>
+        <BioSection>
+          <BioInfo>2017</BioInfo>
+          Completed Bachelor of Secience (B.S.) Aeronautics & Information
+          Systems from University of North Dakota
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Photography, Learning, Design, Playing Videos Games
+        </Paragraph>
+      </Section>
     </Container>
   );
 };
